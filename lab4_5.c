ea9a2daca894ba4a341842c826c2674afdd9f210
Peerawat Wongmek <peerawat.won@ku.th>
2021-03-01 18:33:48 +0700

    as

--- lab4_5.c
+++ lab4_5.c
@@ -5,31 +5,27 @@ int main()
     int n,i,j,tmp,k;
     int r[4];
     scanf("%d",&n);
-    int ans[n];
-    char s[n][51];
-    for(i=1; i<4; i++)
-        r[i]=0;
-    r[0]=1;
-    for(i=0; i<n; i++)
-        scanf("%s",s[i]);
-    printf("%d",strlen(s[0]));
-    for(i=0; i<n; i++)
-    {
-        for(j=0; j<strlen(s[i]); j++)
+    char s[51];
+    for(i=0; i<n; i++){
+        scanf("%s",s);
+        for(k=1; k<4; k++)
+            r[k]=0;
+        r[0]=1;
+        for(j=0; j<strlen(s); j++)
         {
-            if(s[i][j]=='A')
+            if(s[j]=='A')
             {
                 tmp=r[1];
                 r[1]=r[2];
                 r[2]=tmp;
             }
-            else if(s[i][j]=='B')
+            else if(s[j]=='B')
             {
                 tmp=r[0];
                 r[0]=r[3];
                 r[3]=tmp;
             }
-            else if(s[i][j]=='C')
+            else if(s[j]=='C')
             {
                 tmp=r[0];
                 r[0]=r[2];
@@ -38,7 +34,7 @@ int main()
                 r[1]=r[3];
                 r[3]=tmp;
             }
-            else if(s[i][j]=='D')
+            else if(s[j]=='D')
             {
                 tmp=r[0];
                 r[0]=r[1];
@@ -47,7 +43,7 @@ int main()
                 r[2]=r[3];
                 r[3]=tmp;
             }
-            else if(s[i][j]=='E')
+            else if(s[j]=='E')
             {
                 tmp=r[0];
                 r[0]=r[3];
@@ -57,9 +53,15 @@ int main()
                 r[2]=tmp;
             }
         }
-        printf("%d %d %d %d",r[0],r[1],r[2],r[3]);
-    }
+         for(k=0; k<4; k++)
+         {
+            if(r[k]==1)
+            {
+                printf("%d\n",k+1);
+            }
 
+         }
+    }
 
     return 0;
 }
